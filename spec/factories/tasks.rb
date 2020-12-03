b9acc4e676b892775a437651b267f3cb07ef453e
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-12-03 14:21:19 +0900

    Add: タスク一覧表示機能のテストを追加

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factor:task do
+  factory :task do
     name {'テストを書く'}
     description {'RSpec & Capybara & FactoryBotを準備する'}
     user
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factor:user do
+  factory :user do
     name {'テストユーザー'}
     email {'test1@example.com'}
     password {'password'}
